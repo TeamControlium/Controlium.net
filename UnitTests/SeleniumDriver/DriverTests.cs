89d844de20b49b0b55b7b2f75eb3d6f2cda763bf
Mat Walker <v-mwalk@users.noreply.github.com>
2018-05-24 07:25:05 +1000

    Assert comment typo
    Checking the process count is > 0 so one or more...

--- UnitTests/SeleniumDriver/DriverTests.cs
+++ UnitTests/SeleniumDriver/DriverTests.cs
@@ -55,8 +55,8 @@ namespace TeamControlium.Controlium.UnitTests.SeleniumDriver
                 var driver = new Controlium.SeleniumDriver();
 
                 // Assert 
-                Assert.IsTrue(Process.GetProcessesByName(browser.ServerProcessName).Count() > 0, $"There must be more than one process named ({browser.ServerProcessName})");
-                Assert.IsTrue(Process.GetProcessesByName(browser.ProcessName).Count() > 0, $"There must be more than one process named ({browser.ProcessName})");
+                Assert.IsTrue(Process.GetProcessesByName(browser.ServerProcessName).Count() > 0, $"There must be one or more processes named ({browser.ServerProcessName})");
+                Assert.IsTrue(Process.GetProcessesByName(browser.ProcessName).Count() > 0, $"There must be one or more processes named ({browser.ProcessName})");
 
                 // Antiseptic
                 driver.CloseDriver();
