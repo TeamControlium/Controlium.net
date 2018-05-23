39098d32270fe8e33631c7b68b201e577e8b0c17
Mike B <maddogmikeb@users.noreply.github.com>
2018-05-23 14:41:32 +1000

    Tidy Up
    Tidy up and better specflow usage

---
+++ UnitTests/SeleniumDriver/DriverTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+using TeamControlium.Utilities;
+
+namespace TeamControlium.Controlium.UnitTests.SeleniumDriver
+{
+    [TestClass]
+    public class DriverTests
+    {
+        List<dynamic> browsers = new List<dynamic>() {
+                new {
+                    Name = "IE11",
+                    ProcessName = "iexplore",
+                    ServerProcessName = "IEDriverServer"
+                },
+                new {
+                    Name = "Chrome",
+                    ProcessName = "chrome",
+                    ServerProcessName = "chromedriver"
+                }
+            };
+
+        [TestInitialize]
+        public void Init()
+        {
+            TestData.Repository.Clear();
+            Controlium.SeleniumDriver.ResetSettings();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            TestData.Repository.Clear();
+            Controlium.SeleniumDriver.ResetSettings();
+        }
+
+        [TestMethod]
+        public void Selenium_can_be_launched_with_mandatory_settings_set()
+        {
+            foreach (dynamic browser in browsers)
+            {
+                // Arrange             
+                Init();
+
+                KillProcess(browser.ServerProcessName);
+                KillProcess(browser.ProcessName);
+
+                TestData.Repository["Selenium", "Browser"] = browser.Name;
+                TestData.Repository["Selenium", "Host"] = "localhost";
+                TestData.Repository["Selenium", "SeleniumServerFolder"] = ".//..//..//TestSeleniumServer";
+                TestData.Repository["Selenium", "DebugMode"] = "off";
+
+                // Act
+                var driver = new Controlium.SeleniumDriver();
+
+                // Assert 
+                Assert.IsTrue(Process.GetProcessesByName(browser.ServerProcessName).Count() > 0, $"There must be more than one process named ({browser.ServerProcessName})");
+                Assert.IsTrue(Process.GetProcessesByName(browser.ProcessName).Count() > 0, $"There must be more than one process named ({browser.ProcessName})");
+
+                // Antiseptic
+                driver.CloseDriver();
+                driver = null;
+
+                CleanUp();
+            }
+        }
+
+        private void KillProcess(string processNameToKill)
+        {
+            Process[] processes = Process.GetProcessesByName(processNameToKill);
+            foreach (var process in processes)
+            {
+                try
+                {
+                    process.Kill();
+                    if (!process.WaitForExit(10000))
+                    {
+                        Assert.Inconclusive($"Killing {processNameToKill}] (ID = {process.Id}) did not work within 10 seconds");
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
