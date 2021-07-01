ec6d566848f34e75ffa2489fbf751d3831cdb8db
Mat Walker <mat@alexview.com>
2021-07-01 14:42:24 +1000

    Completed .NET5.0 migration.  Tests migrated as well but need many many more unit tests!

---
+++ Controlium.UnitTests.net/Features/SeleniumDriver.feature
@@ -0,0 +1,44 @@
+﻿Feature: SeleniumDriverTests
+
+Background: 
+    Given There are no processes running named "microsoftwebdriver"
+    And There are no processes running named "msedge"
+    And There are no processes running named "chromedriver"
+    And There are no processes running named "chrome"
+    And setting Category "Selenium", Option "Host" is "localhost"
+    And setting Category "Selenium", Option "SeleniumServerFolder" is ".//..//..//..//TestSeleniumServer"
+    And setting Category "Selenium", Option "DebugMode" is "off"
+
+Scenario Outline: Selenium can be launched with mandatory settings set
+    Given setting Category "Selenium", Option "Browser" is <Browser>
+    When I instantiate SeleniumDriver
+    Then  a process exists named <Server process name>
+    And a process exists named <Process name>
+
+Examples:
+    | Browser  | Process name | Server process name  |
+    | "Edge"   | "msedge"     | "microsoftwebdriver" |
+    | "Chrome" | "chrome"     | "chromedriver"       |
+
+Scenario Outline: When I use SeleniumDriver to browse to Google Search I get the correct page title
+    Given Setting Category "Selenium", Option "Browser" is <Browser>
+    And I instantiate SeleniumDriver
+    When I browse to "http://www.google.com"
+    Then I can read the page title "Google"
+
+Examples:
+    | Browser  |
+    | "Edge"   |
+    | "Chrome" |
+
+Scenario Outline: I can find an element on the page from the top level
+    Given setting Category "Selenium", Option "Browser" is <Browser>
+    And I instantiate SeleniumDriver
+    And I browse to "http://www.google.com"
+    When I use FindElement to locate an element using XPath "//input[@name='q']"
+    Then a valid element is found
+
+Examples:
+    | Browser  |
+    | "Edge"   |
+    | "Chrome" |
---
+++ Controlium.UnitTests.net/Steps/ControlBaseTestSteps.cs
@@ -0,0 +1,70 @@
+﻿using Internal.Tester;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamControlium.Utilities;
+using TechTalk.SpecFlow;
+
+namespace TeamControlium.Controlium
+{
+    [Binding]
+    public sealed class ControlBaseTestSteps
+    {
+        /// <summary>
+        /// Used to hold context information for current Scenario
+        /// </summary>
+        private readonly ScenarioContext scenarioContext;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ControlBaseTestSteps" /> class.
+        /// Used by Specflow supply Scenario context information.
+        /// </summary>
+        /// <param name="context">Scenario context data for use by class methods</param>
+        public ControlBaseTestSteps(ScenarioContext context)
+        {
+            this.scenarioContext = context;
+        }
+
+
+        [Given(@"I am using local browser ""(?i)(.*)""")]
+        public void GivenIAmUsingBrowser(string browser)
+        {
+            Repository.SetItemGlobal("Selenium", "Browser",browser);
+            Repository.SetItemGlobal("Selenium", "Host","localhost");
+            Repository.SetItemGlobal("Selenium", "SeleniumServerFolder",".//..//..//..//TestSeleniumServer");
+            Repository.SetItemGlobal("Selenium", "DebugMode","off");
+        }
+
+        [Given(@"I instantiate SeleniumDriver and browse to ""(.*)""")]
+        public void WhenIInstantiateSeleniumDriver(string urlToBrowseTo)
+        {
+            var driver = new SeleniumDriver();
+            driver.GotoURL(urlToBrowseTo);
+            scenarioContext.Add("SeleniumDriver", driver);
+        }
+
+        [When(@"I find Textbox control with title ""(.*)""")]
+        public void WhenIFindFKTextboxControlWithTitle(string controlTitle)
+        {
+            var driver = scenarioContext.Get<SeleniumDriver>("SeleniumDriver");
+            var control = driver.SetControl(new ControlBaseTester(controlTitle));
+            scenarioContext.Add("Control", control);
+        }
+
+        [When(@"enter text ""(.*)""")]
+        public void WhenEnterText(string textToEnter)
+        {
+            scenarioContext.Get<ControlBaseTester>("Control").Text = textToEnter;
+        }
+
+        [Then(@"Control is found")]
+        public void ThenControlIsFound()
+        {
+            Assert.IsNotNull(scenarioContext.Get<ControlBaseTester>("Control"));
+        }
+
+        [Then(@"I can read the text ""(.*)""")]
+        public void ThenICanReadTheText(string stringExpected)
+        {
+            Assert.AreEqual(scenarioContext.Get<ControlBaseTester>("Control").Text, stringExpected);
+        }
+    }
+}
